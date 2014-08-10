a6581fb8b1da28db47065ac19ac02b2329db2e28
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-08-10 15:32:00 +0900

    Create cli.Command struct in subcommands' main src

--- cmd/yurufuwa/main.go
+++ cmd/yurufuwa/main.go
@@ -11,17 +11,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "yurufuwa"
 	app.Usage = "Admin tools to manage Yurufuwa organization."
-
-	client := yurufuwa.CreateClient()
-	app.Commands = []cli.Command{
-		{
-			Name:  "members",
-			Usage: "Show yurufuwa members.",
-			Action: func(c *cli.Context) {
-				yurufuwa.ListMembers(client)
-			},
-		},
-	}
+	app.Commands = []cli.Command{*yurufuwa.MembersCommand()}
 
 	app.Action = func(c *cli.Context) {
 		cli.ShowAppHelp(c)
--- members.go
+++ members.go
@@ -3,11 +3,23 @@ package yurufuwa
 import (
 	"fmt"
 
+	"github.com/codegangsta/cli"
 	"github.com/google/go-github/github"
 )
 
-// ListMembers lists yurufuwa orgnization members.
-func ListMembers(client *github.Client) {
+// MembersCommand makes members subcommand.
+func MembersCommand() *cli.Command {
+	return &cli.Command{
+		Name:  "members",
+		Usage: "Show yurufuwa members.",
+		Action: func(c *cli.Context) {
+			client := CreateClient()
+			listMembers(client)
+		},
+	}
+}
+
+func listMembers(client *github.Client) {
 	members, _, err := client.Organizations.ListMembers("yurufuwa", &github.ListMembersOptions{})
 
 	if err != nil {
